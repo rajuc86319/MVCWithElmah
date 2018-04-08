f113217ea4b6f131b38c5b29d46543ab0ef02708
rajuc86319 <raju86319@gmail.com>
2018-04-08 14:21:05 +0530

    done some changes

--- RadianSampleTask/RegistrationTaskMVC/Areas/MvcElmahDashboard/Controllers/HomeController.cs
+++ RadianSampleTask/RegistrationTaskMVC/Areas/MvcElmahDashboard/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace RegistrationTaskMVC.Areas.MvcElmahDashboard.Controllers
         }
 
         #endregion
-
+//local changes
         // GET: ElmahLog/Home
         public ActionResult Index()
         {
